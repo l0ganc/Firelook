811008f789a7a7564762bf9f0558a5f243ec83ff
l0ganc <jingyige@usc.edu>
2018-10-29 20:51:58 -0700

    add comments to events

--- app/src/main/java/com/logan/eventreporter/EventListAdapter.java
+++ app/src/main/java/com/logan/eventreporter/EventListAdapter.java
@@ -1,6 +1,7 @@
 package com.logan.eventreporter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
@@ -12,8 +13,11 @@ import android.widget.TextView;
 
 import com.google.android.gms.ads.AdLoader;
 import com.google.android.gms.ads.NativeExpressAdView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -76,7 +80,16 @@ public class EventListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
         public TextView description;
         public TextView time;
         public ImageView imgview;
+
+        public ImageView img_view_good;
+        public ImageView img_view_comment;
+
+        public TextView good_number;
+        public TextView comment_number;
+
         public View layout;
+
+
 
         public ViewHolder(View v) {
             super(v);
@@ -86,6 +99,13 @@ public class EventListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
             description = (TextView) v.findViewById(R.id.event_item_description);
             time = (TextView) v.findViewById(R.id.event_item_time);
             imgview = (ImageView) v.findViewById(R.id.event_item_img);
+
+            img_view_good = (ImageView) v.findViewById(R.id.event_good_img);
+            img_view_comment = (ImageView) v.findViewById(R.id.event_comment_img);
+            good_number = (TextView) v.findViewById(R.id.event_good_number);
+            comment_number = (TextView) v.findViewById(R.id.event_comment_number);
+
+
         }
     }
 
@@ -164,6 +184,8 @@ public class EventListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
         holder.description.setText(event.getDescription());
         holder.time.setText(Utils.timeTransformer(event.getTime()));
 
+        holder.good_number.setText(String.valueOf(event.getLike()));
+
 
         if (event.getImgUri() != null) {
             final String url = event.getImgUri();
@@ -182,7 +204,48 @@ public class EventListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
         } else {
             holder.imgview.setVisibility(View.GONE);
         }
+
+        holder.img_view_good.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference.child("events").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            Event recordedevent = snapshot.getValue(Event.class);
+                            if (recordedevent.getId().equals(event.getId())) {
+                                int number = recordedevent.getLike();
+                                holder.good_number.setText(String.valueOf(number + 1));
+                                snapshot.getRef().child("like").setValue(number + 1);
+                                break;
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+
+        holder.layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, CommentActivity.class);
+                String eventId = event.getId();
+                intent.putExtra("EventID", eventId);
+                context.startActivity(intent);
+            }
+        });
+
+
+
     }
+
+
 
     private void configureAdsView(final ViewHolderAds adsHolder, final int position) {
         ViewHolderAds nativeExpressHolder =
